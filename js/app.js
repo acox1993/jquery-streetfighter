453e65c39773cab683aae0627d79aec3c40d7660
acox1993 <alexandriacrmu@gmail.com>
2016-05-05 10:36:39 -0400

    add still to ready and back

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
-	$('#ready').hide();
-	$('#cool').hide();
-	$('#throw').hide();
-
+	// $('#ready').hide();
+	// $('#cool').hide();
+	// $('#throw').hide();
+	$('#fighter-box').mouseenter(function() {
+		$('#still').hide();
+		$('#ready').show();
+	})
+	    .mouseleave(function() {
+         $('#ready').hide();
+         $('#still').show();
+     });
 });
